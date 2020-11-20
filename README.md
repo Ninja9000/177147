bbae121e4e39b8620ca9aaa2586a7283b5153e68
Ninja9000 <ninjacob9000@gmail.com>
2020-11-19 22:09:40 -0500

    Hopefully adds my lazy tilegen

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 177147
 177147 is a parody of the game 2048, a clone of a game of the same name, which, in turn, was derived from the game 1024!
 
-This game is different in that it centers around 3, both in having powers of 3 replace powers of 2 and by having three axes of movement.
+This game is different in that it uses three as a numerical motif, with tiles having powers of three and with three axes of movement.
 
 Expect badness and madness to ensue.
+
+Extremely cursed code, with credit to [ced-h](https://github.com/cedric-h).
--- main.js
+++ main.js
@@ -1,22 +1,95 @@
+//Needs neighbor checks and good grid math.
+
 var c = document.getElementById("gameCanvas");
 var ctx = c.getContext('2d');
-c.height = window.innerHeight;
 c.width = window.innerWidth;
+c.height = window.innerHeight;
+var gridW = window.innerWidth;
+var gridH = window.innerHeight;
 
-function Tile(x, y, c) {
-    this.value = Math.pow(3, Math.round(Math.random()) + 1);
+ctx.fillStyle = "darkgray";
+ctx.beginPath();
+ctx.moveTo(gridW / 2, gridH / 2 - 106 * Math.sqrt(3) - 4);
+ctx.lineTo(gridW / 2 - 152, gridH / 2 + 34 * Math.sqrt(3) - 1);
+ctx.lineTo(gridW / 2 + 152, gridH / 2 + 34 * Math.sqrt(3) - 1);
+ctx.closePath();
+ctx.fill();
+
+function Tile(x, y) {
+    this.value = 0;
     this.pos = [x, y];
-    this.color = c;
-    this.tileDraw = function() {
+    this.color = "lightgray";
+
+    this.draw = function() {
+        var evenSpace = (this.pos[0] + this.pos[1]) % 2 == 0;
+        
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.moveTo(this.pos[0], this.pos[1] + 30 * Math.sqrt(3));
-        ctx.lineTo(this.pos[0] - 30, this.pos[1]);
-        ctx.lineTo(this.pos[0] + 30, this.pos[1]);
+        
+        if (evenSpace) {
+            ctx.moveTo(gridW / 2 + this.pos[0] * 37, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3) + 30 * Math.sqrt(3));
+            ctx.lineTo(gridW / 2 + this.pos[0] * 37 - 30, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3));
+            ctx.lineTo(gridW / 2 + this.pos[0] * 37 + 30, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3));
+        } else {
+            ctx.moveTo(gridW / 2 + this.pos[0] * 37, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3));
+            ctx.lineTo(gridW / 2 + this.pos[0] * 37 - 30, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3) + 30 * Math.sqrt(3));
+            ctx.lineTo(gridW / 2 + this.pos[0] * 37 + 30, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3) + 30 * Math.sqrt(3));
+        }
+
         ctx.closePath();
         ctx.fill();
+
+        if (this.value != 0) {
+            ctx.fillStyle = "gray";
+            ctx.font = "bold 18pt Arial";
+
+            if (evenSpace) {
+                ctx.fillText(this.value, gridW / 2 + this.pos[0] * 37 - 7, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3) + 16 * Math.sqrt(3));
+            } else {
+                ctx.fillText(this.value, gridW / 2 + this.pos[0] * 37 - 7, gridH / 2 - this.pos[1] * 34 * Math.sqrt(3) + 24 * Math.sqrt(3));
+            }
+        }
+    };
+    
+    this.recolor = function() {
+        this.color = "white";
+    }
+
+    this.realize = function() {
+        this.value = Math.pow(3, Math.round(Math.random()) + 1);
+        this.recolor();
+        this.draw();
+    }
+
+    this.getPos = function() {
+        return this.pos;
     };
 };
+
+var tiles = [];
+let i = 0;
+while (i < 17) {
+    for (let x = -3; x < 4; x++) {
+        tiles[i] = new Tile(x, 0);
+        tiles[i].draw();
+        i++;
+    }
+    for (let x = -2; x < 3; x++) {
+        tiles[i] = new Tile(x, 1);
+        tiles[i].draw();
+        i++;
+    }
+    for (let x = -1; x < 2; x++) {
+        tiles[i] = new Tile(x, 2);
+        tiles[i].draw();
+        i++;
+    }
+    tiles[i] = new Tile(0, 3);
+    tiles[i].draw();
+}
+
+tiles[Math.round(Math.random() * 16)].realize();
+tiles[Math.round(Math.random() * 16)].realize();
 
 window.requestAnimFrame = (function() {
     return window.requestAnimationFrame ||
